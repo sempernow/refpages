dd3c20775855ecfff55b10e680a9923b8b33e2ab
dev <sempernow.com@gmail.com>
2024-08-25 21:57:22 -0400

    REF.GitOps.md @ 2024-08-25T21:14Z

--- REFs/K8s.kubectl.sh
+++ REFs/K8s.kubectl.sh
@@ -2,93 +2,57 @@
 #------------------------------------------------------------------------------
 #  Kubernetes tools : kubectl
 # -----------------------------------------------------------------------------
-
-
-# jsonpath : https://kubernetes.io/docs/reference/kubectl/jsonpath/
-kubectl get node -o jsonpath={.items[*].spec.podCIDRs}
-# ["10.240.0.0/24"] ["10.240.1.0/24"] ["10.240.2.0/24"] ["10.240.3.0/24"]
-kubectl get node -o jsonpath='{range .items[*]}{.spec.podCIDRs}{"\n"}{end}'
-## template : equivalent
-kubectl get node -o template='{{range .items}}{{.spec.podCIDRs}}{{"\n"}}{{end}}'
-# ["10.240.0.0/24"]
-# ["10.240.1.0/24"]
-# ["10.240.2.0/24"]
-# ["10.240.3.0/24"]
-
-# Patch : https://chatgpt.com/share/a45d346d-270e-4919-94a7-dccabb1e1246
-## JSON Patch (RFC 6902)
-kubectl patch $kind $name --type='json' \
-    -p='[{"op": "replace", "path": "/spec/replicas", "value": 3}]'
-## JSON Merge Patch (RFC 7396) : concise syntax
-kubectl patch $kind $name --type='merge' \
-    -p='{"spec": {"replicas": 3}}'
-## K8s Strategic Merge Patch
-kubectl patch $kind $name --type='strategic' \
-    -p='{"spec": {"template": {"spec": {"containers": [{"name": "nginx", "image": "nginx:1.15.4"}]}}}}'
-## Using Kustomize / YAML 
-kustomize build $folder | kubectl apply -f -
-## Equivalent:
-kubectl kustomize $folder | kubectl apply -f -
-
-# Managed Kubernetes Environment @ Google Cloud Platform 
-## Kubernetes Engine > Cluster > Create > GKE Standard
-
-# Bash config : kubectl completion 
+# Shell config : kubectl completion 
 set +o posix # else redirect of process substitution (@script) fails.
 source <(kubectl completion bash)
-# Bash config : k completion 
+# Shell config : k completion 
 alias k=kubectl
 complete -o default -F __start_kubectl k
 
-# Usage info per command
+# Help
 kubectl $command -h |less
-
-## Explain : Spec (YAML) node (sub)field
 kubectl explain $_OBJECT.$_FIELD.$_SUB_FIELD
+kubectl explain pod.spec.containers.securityContext
 kubectl explain pod.metadata
 kubectl explain pod.spec.containers.volumeMounts
 kubectl explain --recursive deployment.spec.strategy
+kubectl api-resources # List all K8s objects, i.e., kind: OBJECT
 
-# Pull file(s) from container to local path 
-ctnr=pname-7bb6f649c6-stl26
-from=/usr/share/nginx/html/index.html
-to_local=index.pulled.html
-kubectl cp $ctnr:$from $to_local
-
-# Workflow
-kubectl create deploy $dname --image=$img
-kubectl scale deploy $dname --replicas=3 
-# OR
-kubectl apply -f . # Process all YAML files in $PWD
-kubectl get all [-ns $ns] 
-kubectl get pods   # Monitor the startup process
+# Manage workloads
+kubectl apply -f . # Create/Update all objects of all manifests in PWD 
+#... lest mod(s) of immutable key(s) if update.
+kubectl create deploy $any --image=$img
+kubectl scale deploy $any --replicas=3 
+kubectl get all -n kube-system # 'all' is *not* all : See `kubectl api-resources` 
+all='pod,deploy,ds,sts,svc,ingress,cm,secret,pvc,pv'
+kubectl get $all -A # Across all namespaces
+kubectl get pods -o wide # Monitor the startup process including node
+## Get all pod names of this namespace
+kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+## Get all images running in this cluster
+kubectl get pods -A -o jsonpath='{range .items[*]}{.spec.containers[*].image}{"\n"}{end}'
 ## Get all Pods having Label/Selector (--selector KEY=VAL, -l KEY=VAL)
-kubectl get po -l type=canary -o json |jq . > k.get.po-l_type_canary.json
+kubectl get po -l type=canary
 ## Get the keys 'name' and 'podID' of all Pods so labelled.
 kubectl get po -l type=canary -o json |jq '.items[] | .metadata.name,.status.podIP'
 ## Get using JSONPath : List node names, one per line.
-k get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
 
-kubectl get deploy $dname -o yaml |less  # Get deployment details
-kubectl edit deploy $dname               # Edit (vi; limited per K8s rules)
-kubectl describe pod $podName  # Examine the pod's (current) State / Reason, and Last State / Reason
-kubectl logs $podName          # Examine the APPLICATIONs logs.
-kubectl delete pods $podName
+kubectl get deploy $any -o yaml |less  # Get deployment details
+kubectl edit deploy $any               # Edit any mutable key(s) of any existing object.
+kubectl describe pod $any  # Examine State / Reason, and Last State / Reason
+kubectl delete pods $any
 
-# Examine Pods
-## Describe : pod : get the pod info stored in etcd database
-kubectl describe pod $podName # -o json|yaml |less
-## Describe : any object
-kubectl describe ns $ns
-kubectl describe pods $podName
+# Examine a Pod status
+kubectl describe pod $any |less
     ###  Containers:
     ###    ...
     ###    State: Waiting
     ###      Reason: PodInitializing
     ###  Events:
-
-## Get pod name (POD_NAME) : E.g., 'kubernetes-dashboard-8665bfb777-vx5z2'
-export POD_NAME=$(kubectl get pods -n default -l "app.kubernetes.io/name=kubernetes-dashboard,app.kubernetes.io/instance=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
+# Examine container logs
+kubectl logs $any # If multi-container pod, 
+#... then TAB/select for (required) ctnr name  
 
 ## Connect : launch shell into container 
 kubectl exec -it $pod -- sh # /bin/bash instead of sh, if available
@@ -167,7 +131,7 @@ kubectl get all -A    # All namespaces; --all-namespaces
 all='po,deploy,rs,sts,ep,svc,ingress,pvc,pv'
 kubectl get $all # Larger subset of all K8s objects
 
-# Labels & Selectors : List & Watch FILTERing
+# Labels & Selectors : List & Watch : filter
 ## Equality-based
 kubectl get pods -l environment=production,tier=frontend
 ## Set-based (more expressive; allows for logic)
@@ -185,25 +149,53 @@ kubectl get all
 # Delete the pod
 kubectl delete pod $appName
 
-# Generate kubeconfig file (YAML)
-## Generate YAML per DOCs (copy/paste) : kubernetes.io/docs : pods (sample YAML)
-## OR
-## Generate YAML per `kubectl run ...`
-# Generate YAML (only at single-container pod)
-kubectl run $appName --image=$appImage --dry-run=client -o yaml |tee $appYAML
-##... add custom run command : `-- ...` MUST BE LAST ARG(s)
-kubectl run $appName --image=$appImage \
-    --dry-run=client -o yaml -- sleep 3600 > $appYAML
-## Generate YAML per DOCs : kubernetes.io/docs : pods (sample YAML)
+# Generate manifest (YAML)
+## - Using kubernetes.io/docs : cut/paste from examples
+## - Using `kubectl run...--dry-run=client -o yaml`
+kubectl run bbox --image=busybox --dry-run=client -o yaml -- sleep 1d |tee bbox-pod.yaml
+## - Using `kubectl get ...-o yaml`
+kubectl get deploy ngx -o yaml |tee ngx-deploy.yaml
 
 # Nodes
-kubectl get nodes
-kubectl drain $node --ignore-daemonsets --force
+kubectl get nodes -o wide
+kubectl cordon node $hostname # Prevent new pods from being scheduled to this node.
+kubectl drain $node --ignore-daemonsets --force # Remove all pods from this node lest of ds.
+# jsonpath : https://kubernetes.io/docs/reference/kubectl/jsonpath/
+kubectl get node -o jsonpath={.items[*].spec.podCIDRs}
+# ["10.240.0.0/24"] ["10.240.1.0/24"] ["10.240.2.0/24"] ["10.240.3.0/24"]
+kubectl get node -o jsonpath='{range .items[*]}{.spec.podCIDRs}{"\n"}{end}'
+## template : equivalent
+kubectl get node -o template='{{range .items}}{{.spec.podCIDRs}}{{"\n"}}{{end}}'
+# ["10.240.0.0/24"]
+# ["10.240.1.0/24"]
+# ["10.240.2.0/24"]
+# ["10.240.3.0/24"]
+
+# Patch : https://chatgpt.com/share/a45d346d-270e-4919-94a7-dccabb1e1246
+## JSON Patch (RFC 6902)
+kubectl patch $kind $name --type='json' \
+    -p='[{"op": "replace", "path": "/spec/replicas", "value": 3}]'
+## JSON Merge Patch (RFC 7396) : concise syntax
+kubectl patch $kind $name --type='merge' \
+    -p='{"spec": {"replicas": 3}}'
+## K8s Strategic Merge Patch
+kubectl patch $kind $name --type='strategic' \
+    -p='{"spec": {"template": {"spec": {"containers": [{"name": "nginx", "image": "nginx:1.15.4"}]}}}}'
+## Using Kustomize / YAML 
+kustomize build $folder |kubectl apply -f -
+## Equivalent:
+kubectl kustomize $folder |kubectl apply -f -
+
+# Pull file from container to (local) host path 
+ctnr=pname-7bb6f649c6-stl26
+from=/usr/share/nginx/html/index.html
+to_local=index.pulled.html
+kubectl cp $ctnr:$from $to_local
 
 # API Server info @ minikube
 kubectl -n kube-system describe pod kube-apiserver-minikube 
 
-# API Access
+# API Access : RBAC 
 ## Role : scoped to namespace 
 ### Allow get, watch, and list on pods
 name='a-role-name'
@@ -227,46 +219,59 @@ kubectl create rolebinding $name --role=$role_name --serviceaccount=acme:myapp -
 ## ClusterRoleBinding 
 kubectl create clusterrolebinding $name --clusterrole=$cluserrole_name --user=$user
 
-# kubeconfig : Configure access to multiple clusters, users, and contexts 
+# kubeconfig 
 ## https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/
-## SET clusters 
-kubectl config --kubeconfig=$kubeconfig_file set-cluster $cluster_name_1 \
+## Explicitly 
+config=/path/to/any/valid/kubectl/config
+kubectl --kubeconfig=$config ... 
+## Else 
+export KUBECONFIG=$config
+kubectl ... # Implicit
+## Else
+~/.kube/config 
+kubectl ... # Implicit
+## Merge multiple kubeconfig (contexts)
+export KUBECONFIG=$pathConf1:$pathConf2:$pathConf3
+kubectl config view --flatten |tee /path/to/new/merged/kubeconfig
+kubectl use-context $context # Set context 
+## Get clusters
+kubectl config get-clusters
+## Set clusters 
+kubectl config set-cluster $cluster_name_1 \
     --server=https://192.168.0.100:8443 \
     --certificate-authority=$ca_file
-kubectl config --kubeconfig=$kubeconfig_file set-cluster $cluster_name_2 \
+kubectl config set-cluster $cluster_name_2 \
     --server=https://10.0.111.123:6443 \
     --insecure-skip-tls-verify 
-## UNSET cluster 
-kubectl --kubeconfig=$kubeconfig_file config unset clusters.$cluster_name
-## SET users
-kubectl config --kubeconfig=$kubeconfig_file set-credentials $user_name_1 \
+## Unset cluster 
+kubectl  config unset clusters.$cluster_name
+## Set users
+kubectl config set-credentials $user_name_1 \
     --client-certificate=$client_cert \
     --client-key=$client_key
-kubectl config --kubeconfig=$kubeconfig_file set-credentials $user_name_2 \
+kubectl config set-credentials $user_name_2 \
     --username=$creds_username \
     --password=$creds_password # Instead of user creds here, use client-go credential plugin
     # https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/
-## UNSET user
-kubectl --kubeconfig=$kubeconfig_file config unset users.$user_name 
-## SET contexts
-kubectl config --kubeconfig=$kubeconfig_file set-context $context_1 \
+## Unset user
+kubectl config unset users.$user_name 
+## Set contexts
+kubectl set-context $context_1 \
     --cluster=$cluster_name_1 \
     --namespace=$ns_1 \
     --user=$user_name_1
-kubectl config --kubeconfig=$kubeconfig_file set-context $context_2 \
+kubectl config set-context $context_2 \
     --cluster=development \
     --namespace=$ns_2 \
     --user=$user_name_1
-kubectl config --kubeconfig=$kubeconfig_file set-context $context_3 \
+kubectl config set-context $context_3 \
     --cluster=$cluster_name_2 \
     --namespace=$ns_3 \
     --user=$user_name_2
-## UNSET context
-kubectl --kubeconfig=$kubeconfig_file config unset contexts.$context_name_1
-
+## Unset context
+kubectl config unset contexts.$context_name_1
 # Switch between clusters
 kubectl config use-context $cluster_name
-
 ## Switch between namespaces 
 kubectl config set-context --current --namespace=$ns
 #kubens $ns # OTHER UTILITY : https://github.com/ahmetb/kubectx
